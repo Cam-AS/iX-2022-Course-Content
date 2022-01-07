96f6e67b7b31d33a68497ab2f64a74837fda66a6
Cam <62012343+Cam-AS@users.noreply.github.com>
2022-01-07 16:34:47 +0200

    Updated REST API example

--- Day_6/rest-api/app.js
+++ Day_6/rest-api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -17,4 +18,12 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(3000);
+mongoose
+  .connect(
+    'mongodb+srv://cameron:uQViFz9xAE02qAO3@cluster0.yg2fe.mongodb.net/Cluster0?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
--- Day_6/rest-api/controllers/feed.js
+++ Day_6/rest-api/controllers/feed.js
@@ -1,20 +1,65 @@
+const Post = require('../models/post');
+
+// exports.getPosts = (req, res, next) => {
+//   // 200 success
+//   res.status(200).json({
+//     posts: [{ title: 'First Post', content: 'This is the first post!!!' }],
+//   });
+// };
+
+// exports.createPost = (req, res, next) => {
+//   const title = req.body.title;
+//   const content = req.body.content;
+//   // 201 created success
+//   res.status(201).json({
+//     message: 'Post created successfully!',
+//     post: {
+//       id: new Date().toISOString(),
+//       title: title,
+//       content: content,
+//     },
+//   });
+// };
+
 exports.getPosts = (req, res, next) => {
-  // 200 success
-  res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!!!' }],
-  });
+  const postQuery = Post.find();
+  let fetchedPosts;
+  postQuery
+    .then((documents) => {
+      fetchedPosts = documents;
+      return Post.count();
+    })
+    .then((count) => {
+      res.status(200).json({
+        message: 'Posts fetched successfully!',
+        posts: fetchedPosts,
+        maxPosts: count,
+      });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: 'Fetching posts failed!',
+      });
+    });
 };
 
 exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  // 201 created success
-  res.status(201).json({
-    message: 'Post created successfully!',
-    post: {
-      id: new Date().toISOString(),
-      title: title,
-      content: content,
-    },
+  const post = new Post({
+    title: title,
+    content: content,
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully!',
+        post: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
